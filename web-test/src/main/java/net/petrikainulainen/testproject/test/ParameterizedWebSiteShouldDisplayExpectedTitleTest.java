c85743712438ef852cc1d6ccee9f8df0c878ea15
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-10-20 21:44:34 +0300

    Update tests based on feedback
    - Describe tests by using the @Test annotation.
    - Set the visibility of the paramter fields to private.
    - Use the WebDriver object provided by the TestProject SDK.

--- web-test/src/main/java/net/petrikainulainen/testproject/test/ParameterizedWebSiteShouldDisplayExpectedTitleTest.java
+++ web-test/src/main/java/net/petrikainulainen/testproject/test/ParameterizedWebSiteShouldDisplayExpectedTitleTest.java
@@ -1,11 +1,12 @@
 package net.petrikainulainen.testproject.test;
 
+import io.testproject.java.annotations.v2.Test;
 import io.testproject.java.annotations.v2.TestParameter;
+import io.testproject.java.sdk.v2.drivers.WebDriver;
 import io.testproject.java.sdk.v2.enums.ExecutionResult;
 import io.testproject.java.sdk.v2.exceptions.FailureException;
 import io.testproject.java.sdk.v2.tests.WebTest;
 import io.testproject.java.sdk.v2.tests.helpers.WebTestHelper;
-import org.openqa.selenium.WebDriver;
 
 /**
  * This test class demonstrates how we can write a simple
@@ -17,13 +18,17 @@ import org.openqa.selenium.WebDriver;
  *     <li>The {@code url} parameter contains the url of the opened website.</li>
  * </ul>
  */
+@Test(
+        name = "The opened website should display the expected title",
+        description = "Verify that the website: {url} displays the title: {expectedTitle}"
+)
 public class ParameterizedWebSiteShouldDisplayExpectedTitleTest implements WebTest {
 
     @TestParameter(defaultValue = "Petri Kainulainen — Developing Software With Passion")
-    public String expectedTitle;
+    private String expectedTitle;
 
     @TestParameter(defaultValue = "https://www.petrikainulainen.net")
-    public String url;
+    private String url;
 
     @Override
     public ExecutionResult execute(WebTestHelper webTestHelper) throws FailureException {
--- web-test/src/main/java/net/petrikainulainen/testproject/test/WebSiteShouldDisplayExpectedTitleTest.java
+++ web-test/src/main/java/net/petrikainulainen/testproject/test/WebSiteShouldDisplayExpectedTitleTest.java
@@ -1,16 +1,21 @@
 package net.petrikainulainen.testproject.test;
 
+import io.testproject.java.annotations.v2.Test;
+import io.testproject.java.sdk.v2.drivers.WebDriver;
 import io.testproject.java.sdk.v2.enums.ExecutionResult;
 import io.testproject.java.sdk.v2.exceptions.FailureException;
 import io.testproject.java.sdk.v2.tests.WebTest;
 import io.testproject.java.sdk.v2.tests.helpers.WebTestHelper;
-import org.openqa.selenium.WebDriver;
 
 /**
  * This test class demonstrates how we can write a simple
  * web test that doesn't require any parameters from the
  * TestProject framework.
  */
+@Test(
+        name = "My website should display the expected title",
+        description= "Verify that my website has the title: Petri Kainulainen — Developing Software With Passion"
+)
 public class WebSiteShouldDisplayExpectedTitleTest implements WebTest {
 
     @Override
