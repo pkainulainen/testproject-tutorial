fb93008d6b2346049fc2ed8c008d5f3c20cfdccd
Petri Kainulainen <petri.kainulainen@gmail.com>
2021-03-13 14:18:16 +0200

    Add proper exception handling to the manual reporting example.

--- opensdk/test-reports/src/test/java/net/petrikainulainen/testproject/opensdk/ManualReportingExampleTest.java
+++ opensdk/test-reports/src/test/java/net/petrikainulainen/testproject/opensdk/ManualReportingExampleTest.java
@@ -2,6 +2,7 @@ package net.petrikainulainen.testproject.opensdk;
 
 import io.testproject.sdk.DriverBuilder;
 import io.testproject.sdk.drivers.web.ChromeDriver;
+import io.testproject.sdk.internal.reporting.ClosableTestReport;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -40,21 +41,23 @@ class ManualReportingExampleTest {
         @Test
         @DisplayName("Should display an empty search result page")
         void shouldDisplayEmptySearchResultPage() {
-            driver.get("https://www.petrikainulainen.net/blog/");
-            driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
+            try (ClosableTestReport testReport = driver.report().test("No search results: should display an empty search result page")) {
+                driver.get("https://www.petrikainulainen.net/blog/");
+                driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
 
-            WebElement searchField = driver.findElement(By.id("s"));
-            driver.report().step("Find the search form from the sidebar");
+                WebElement searchField = driver.findElement(By.id("s"));
+                driver.report().step("Find the search form from the sidebar");
 
-            searchField.sendKeys("pascal");
-            searchField.sendKeys(Keys.ENTER);
-            driver.report().step("Perform the search by using the search term: pascal");
+                searchField.sendKeys("pascal");
+                searchField.sendKeys(Keys.ENTER);
+                driver.report().step("Perform the search by using the search term: pascal");
 
-            WebElement noResultElement = driver.findElement(By.cssSelector(".template-search .content .post_box .archive_content"));
-            assertThat(noResultElement.getText()).isEqualTo("No results found.");
-            driver.report().step("Ensure that the search result page displays the text: No results found.");
+                WebElement noResultElement = driver.findElement(By.cssSelector(".template-search .content .post_box .archive_content"));
+                assertThat(noResultElement.getText()).isEqualTo("No results found.");
+                driver.report().step("Ensure that the search result page displays the text: No results found.");
 
-            driver.report().test("No search results: should display an empty search result page", true).submit();
+                testReport.setResult(true);
+            }
         }
     }
 
@@ -65,48 +68,52 @@ class ManualReportingExampleTest {
         @Test
         @DisplayName("Should display search result page that has one search result")
         void shouldDisplaySearchResultPageWithOneSearchResult() {
-            driver.get("https://www.petrikainulainen.net/blog/");
-            driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
+            try (ClosableTestReport testReport = driver.report().test("One search result: should display search result page that has one search result")) {
+                driver.get("https://www.petrikainulainen.net/blog/");
+                driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
 
-            WebElement searchField = driver.findElement(By.id("s"));
-            driver.report().step("Find the search form from the sidebar");
+                WebElement searchField = driver.findElement(By.id("s"));
+                driver.report().step("Find the search form from the sidebar");
 
-            searchField.sendKeys("clojure");
-            searchField.sendKeys(Keys.ENTER);
-            driver.report().step("Perform the search by using the search term: clojure");
+                searchField.sendKeys("clojure");
+                searchField.sendKeys(Keys.ENTER);
+                driver.report().step("Perform the search by using the search term: clojure");
 
-            List<WebElement> searchResults = driver.findElements(By.tagName("article"));
-            driver.report().step("Find the search results from the search result page");
+                List<WebElement> searchResults = driver.findElements(By.tagName("article"));
+                driver.report().step("Find the search results from the search result page");
 
-            assertThat(searchResults).hasSize(1);
-            driver.report().step("Ensure that the search result page displays one search result");
+                assertThat(searchResults).hasSize(1);
+                driver.report().step("Ensure that the search result page displays one search result");
 
-            driver.report().test("One search result: should display search result page that has one search result", true).submit();
+                testReport.setResult(true);
+            }
         }
 
         @Test
         @DisplayName("Should display search result page that has the correct search result")
         void shouldDisplaySearchResultPageWithCorrectSearchResult() {
-            driver.get("https://www.petrikainulainen.net/blog/");
-            driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
+            try (ClosableTestReport testReport = driver.report().test("One search result: should display search result page that has the correct search result")) {
+                driver.get("https://www.petrikainulainen.net/blog/");
+                driver.report().step("Open the page: https://www.petrikainulainen.net/blog/");
 
-            WebElement searchField = driver.findElement(By.id("s"));
-            driver.report().step("Find the search form from the sidebar");
+                WebElement searchField = driver.findElement(By.id("s"));
+                driver.report().step("Find the search form from the sidebar");
 
-            searchField.sendKeys("clojure");
-            searchField.sendKeys(Keys.ENTER);
-            driver.report().step("Perform the search by using the search term: clojure");
+                searchField.sendKeys("clojure");
+                searchField.sendKeys(Keys.ENTER);
+                driver.report().step("Perform the search by using the search term: clojure");
 
-            WebElement searchResult = driver.findElement(By.tagName("article"));
-            driver.report().step("Find the only search result from the search result page");
+                WebElement searchResult = driver.findElement(By.tagName("article"));
+                driver.report().step("Find the only search result from the search result page");
 
-            WebElement resultTitle = searchResult.findElement(By.className("headline"));
-            driver.report().step("Find the title of the search result");
+                WebElement resultTitle = searchResult.findElement(By.className("headline"));
+                driver.report().step("Find the title of the search result");
 
-            assertThat(resultTitle.getText()).isEqualTo("Java Testing Weekly 22 / 2018");
-            driver.report().step("Ensure that the found search result has the correct title");
+                assertThat(resultTitle.getText()).isEqualTo("Java Testing Weekly 22 / 2018");
+                driver.report().step("Ensure that the found search result has the correct title");
 
-            driver.report().test("One search result: should display search result page that has the correct search result", true).submit();
+                testReport.setResult(true);
+            }
         }
     }
 
